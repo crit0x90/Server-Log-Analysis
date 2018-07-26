3bbeae05c8b6a79e349c89dc08de2c2551c01af2
Scott <scottmccoy13@gmail.com>
2018-07-26 11:52:49 -0700

    Continued debugging and writing the tests, almost done

--- testing/timestamping/test.cpp
+++ testing/timestamping/test.cpp
@@ -10,6 +10,10 @@ using namespace std;
 map<string, int> monthMap;
 map<int, string> reverseMonthMap;
 map<int, int> daycountMap;
+vector<string> dateVector;
+vector<string> timeVector;
+
+
 typedef unsigned long long int TIME_TYPE;
 
 void populateMonthMap()
@@ -60,32 +64,99 @@ void populateDaycountMap()
 	daycountMap.insert(make_pair(12, 31));
 }
 
+void populateDateVector()
+{
+	string currentDate = "";
+	for(int i = 1; i < 13; i++)
+	{
+		for(int j = 1; j < daycountMap[i] + 1; j++)
+		{
+			if(j < 10)
+			{
+				currentDate.append("0");
+			}
+			currentDate.append(to_string(j));
+			currentDate.append("/");
+			currentDate.append(reverseMonthMap[i]);
+			currentDate.append("/2018");
+			//cout << currentDate << endl;
+			dateVector.push_back(currentDate);
+			currentDate = "";
+		}
+	}
+}
+
+void populateTimeVector()
+{
+	string currentTime = ":";
+	for(int i = 0; i < 24; i++)
+	{
+		for(int j = 0; j < 60; j++)
+		{
+			for(int k = 0; k < 60; k++)
+			{
+				//hours
+				if(i < 10)
+				{
+					currentTime.append("0");
+				}
+				currentTime.append(to_string(i));
+				currentTime.append(":");
+
+				//minutes
+				if(j < 10)
+				{
+					currentTime.append("0");
+				}
+				currentTime.append(to_string(j));
+				currentTime.append(":");
+
+				//seconds
+				if(k < 10)
+				{
+					currentTime.append("0");
+				}
+				currentTime.append(to_string(k));
+
+				//cout << currentTime << endl;
+				timeVector.push_back(currentTime);
+				currentTime = ":";
+			}
+		}
+	}
+}
+
 TIME_TYPE toTimeType(string data)
 {
 	//initialization
+	/*
 	typedef vector<string> split_vector_type;
 	//splits the string
 	split_vector_type splitVec;
-	std::string::size_type sz;
 	boost::split( splitVec, data, boost::is_any_of(" "), boost::token_compress_on );
-
 	string datetime = splitVec.at(3);
+	*/
+	std::string::size_type sz;
+	string datetime = data;
+
+	cout << datetime << endl;
 	unsigned long long int timestamp;
-	string year = datetime.substr(10, 2);
-	string month = datetime.substr(4, 3);
-	string day = datetime.substr(1, 2);
+	string year = datetime.substr(9, 2);
+	string month = datetime.substr(3, 3);
+	string day = datetime.substr(0, 2);
 	string hours = datetime.substr(13, 2);
 	string minutes = datetime.substr(16, 2);
 	string seconds = datetime.substr(19,2);
 
-	cout << "Datetime: " << datetime << endl;
-	cout << "Year: " << stoi(year, &sz) << endl;
-	cout << "Month: " << month << endl;
-	cout << "Day: " << stoi(day, &sz) << endl;
-	cout << "Hours: " << stoi(hours, &sz) << endl;
-	cout << "Minutes: " << stoi(minutes, &sz) << endl;
-	cout << "Seconds: " << seconds << endl;
-	
+	/*
+		cout << "Datetime: " << datetime << endl;
+		cout << "Year: " << stoi(year, &sz) << endl;
+		cout << "Month: " << month << endl;
+		cout << "Day: " << stoi(day, &sz) << endl;
+		cout << "Hours: " << stoi(hours, &sz) << endl;
+		cout << "Minutes: " << stoi(minutes, &sz) << endl;
+		cout << "Seconds: " << seconds << endl;
+	*/
 	timestamp = stoi(year, &sz) * 10000000000; //10,000,000,000
 	timestamp = timestamp + (monthMap[month] * 100000000); // 100,000,000
 	timestamp = timestamp + (stoi(day, &sz) * 1000000); //1,000,000
@@ -116,22 +187,22 @@ string toReadableTime(TIME_TYPE integerTime)
 //if anyone ever has to debug this I am sorry for this mess, I owe you a beer
 //this function basically fixes any illegalities in the time formatting
 //(i.e. > 59 seconds in the seconds field)
-TIME_TYPE incrementTimeStamp(TIME_TYPE old_stamp, long int lookahead)
+TIME_TYPE incrementTimeStamp(TIME_TYPE old_stamp, int lookahead)
 {
 	int days = lookahead / 84600;
-	cout << "Days: " << days << endl;
+	//cout << "Days: " << days << endl;
 	lookahead = lookahead % 84600;
 
 	int hours = lookahead / 3600;
-	cout << "Hours: " << hours << endl; 
+	//cout << "Hours: " << hours << endl; 
 	lookahead = lookahead % 3600;
 
 	int minutes = lookahead / 60;
-	cout << "Minutes: " << minutes << endl;
+	//cout << "Minutes: " << minutes << endl;
 	lookahead = lookahead % 60;
 
 	int seconds = lookahead;
-	cout << "Seconds: " << seconds << endl;
+	//cout << "Seconds: " << seconds << endl;
 
 	old_stamp += (days * 1000000);
 	old_stamp += (hours * 10000);
@@ -142,13 +213,13 @@ TIME_TYPE incrementTimeStamp(TIME_TYPE old_stamp, long int lookahead)
 	//but the form may be incorrect. i.e. there may be > 60
 	//hours/minutes/seconds
 	
-	cout << "Incremented derived time: " << old_stamp << endl;
-	cout << "Incremented time: " << toReadableTime(old_stamp) << endl;
+	//cout << "Incremented derived time: " << old_stamp << endl;
+	//cout << "Incremented time: " << toReadableTime(old_stamp) << endl;
 
 	//fix seconds - done
 	if(old_stamp % 100 > 59)
 	{
-		cout << "Fixing seconds" << endl;
+		//cout << "Fixing seconds" << endl;
 		int addMinutes = (old_stamp % 100) / 60;
 		//cout << "Adding " << addMinutes << " minutes" << endl;
 		old_stamp -= (60 * addMinutes);
@@ -158,7 +229,7 @@ TIME_TYPE incrementTimeStamp(TIME_TYPE old_stamp, long int lookahead)
 	//fix minutes - done
 	if((old_stamp % 10000) / 100 > 59)
 	{
-		cout << "Fixing minutes" << endl;
+		//cout << "Fixing minutes" << endl;
 		int addHours = ((old_stamp % 10000) / 100) / 60;
 		//cout << "Adding " << addHours << " hours" << endl;
 		old_stamp -= (6000 * addHours);
@@ -168,7 +239,7 @@ TIME_TYPE incrementTimeStamp(TIME_TYPE old_stamp, long int lookahead)
 	//fix hours - done
 	if((old_stamp % 1000000) / 10000 > 23)
 	{
-		cout << "Fixing hours" << endl;
+		//cout << "Fixing hours" << endl;
 		int addDays = ((old_stamp % 1000000) / 10000) / 24;
 
 		old_stamp -= (240000 * addDays);
@@ -178,7 +249,7 @@ TIME_TYPE incrementTimeStamp(TIME_TYPE old_stamp, long int lookahead)
 	//fix days - done
 	if((old_stamp % 100000000) / 1000000 > daycountMap[((old_stamp % 10000000000) / 100000000)])
 	{
-		cout << "Fixing days" << endl;
+		//cout << "Fixing days" << endl;
 		int addMonths = ((old_stamp % 100000000) / 1000000) / \
 			daycountMap[((old_stamp % 10000000000) / 100000000)];
 		
@@ -189,7 +260,7 @@ TIME_TYPE incrementTimeStamp(TIME_TYPE old_stamp, long int lookahead)
 	//fix months - 
 	if((old_stamp % 10000000000) / 100000000 > 12)
 	{
-		cout << "Fixing months" << endl;
+		//cout << "Fixing months" << endl;
 		int addYears = ((old_stamp % 10000000000) / 100000000) / 13;
 		
 		old_stamp -= (1200000000);
@@ -200,28 +271,69 @@ TIME_TYPE incrementTimeStamp(TIME_TYPE old_stamp, long int lookahead)
 	return old_stamp;
 }
 
+//this function check to make sure that there are no invalidities in the
+//form of the stamp
+void checkStamp(TIME_TYPE stamp)
+{
+	//TESTS GO HERE
+}
+
 int main()
 {
-	string testLine = "IP7379 [yfsOU0T2XLuEfWr] user1624 [31/Dec/2017:23:59:50 -0700]";
+	//single line test
+	/*
+		string testLine = "IP7379 [yfsOU0T2XLuEfWr] user1624 [31/Dec/2017:23:59:50 -0700]";
+		string readableTime;
+		TIME_TYPE CURRENT_STAMP;
+		long int lookahead = 10;
+
+		populateMonthMap();
+		populateReverseMonthMap();
+		populateDaycountMap();
+
+		CURRENT_STAMP = toTimeType(testLine);
+		readableTime = toReadableTime(CURRENT_STAMP);
+
+		cout << endl << "Derived time: " << CURRENT_STAMP << endl;
+		cout << "Readable time: " << readableTime << endl;
+
+		cout << "Lookahead distance: " << lookahead << endl;
+		CURRENT_STAMP = incrementTimeStamp(CURRENT_STAMP, lookahead);
+
+		cout << endl << "Adjusted incremented derived time: " << CURRENT_STAMP << endl;
+		cout << "Adjusted incremented time: " << toReadableTime(CURRENT_STAMP) << endl;
+	*/
+
+	/***complete test***/
+
+	string testLine;
 	string readableTime;
-	TIME_TYPE CURRENT_STAMP;
-	long int lookahead = 10;
+	TIME_TYPE currentStamp;
+	int lookahead;
 
 	populateMonthMap();
 	populateReverseMonthMap();
 	populateDaycountMap();
+	populateDateVector();
+	populateTimeVector();
 
-	CURRENT_STAMP = toTimeType(testLine);
-	readableTime = toReadableTime(CURRENT_STAMP);
-
-	cout << endl << "Derived time: " << CURRENT_STAMP << endl;
-	cout << "Readable time: " << readableTime << endl;
-
-	cout << "Lookahead distance: " << lookahead << endl;
-	CURRENT_STAMP = incrementTimeStamp(CURRENT_STAMP, lookahead);
-
-	cout << endl << "Adjusted incremented derived time: " << CURRENT_STAMP << endl;
-	cout << "Adjusted incremented time: " << toReadableTime(CURRENT_STAMP) << endl;
+	string stamp;
+	TIME_TYPE modifiedStamp;
+	for(int i = 0; i < 84600; i++)
+	{
+		cout << "Iteration " << i << " of 84600" << endl;
+		for(int j = 0; j < dateVector.size(); j++)
+		{
+			for(int k = 0; k < timeVector.size(); k++)
+			{
+				stamp = dateVector.at(j);
+				stamp.append(timeVector.at(k));
+				modifiedStamp = incrementTimeStamp(toTimeType(stamp), i);
+				checkStamp(modifiedStamp);
+			}
+		}	
+	}
+	
 
 	return 0;
 }
